e64cc026eed56c5ec1984f3160496292a4c025e2
iluna007 <146373472+iluna007@users.noreply.github.com>
2025-03-04 21:14:04 +0000

    left only one video

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,38 +1,30 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "../Styles/Home.css";
 import GlitchText from "../components/GlitchText";
 
 const Home = () => {
 	const videoRef = useRef(null);
-	const videos = [
-		"https://res.cloudinary.com/dw1ht0zfd/video/upload/v1740349894/SI_Landing_Page_k1914v.mp4",
-		"https://res.cloudinary.com/dw1ht0zfd/video/upload/v1739293566/Yukon_in_Winter_Chromatic_icotzx.mp4",
-	];
-	const [currentIndex, setCurrentIndex] = useState(0);
+	const videoUrl =
+		"https://res.cloudinary.com/dw1ht0zfd/video/upload/v1740349894/SI_Landing_Page_k1914v.mp4";
 
 	useEffect(() => {
 		const video = videoRef.current;
-		const handleEnded = () => {
-			const nextIndex = (currentIndex + 1) % videos.length;
-			setCurrentIndex(nextIndex);
-			video.src = videos[nextIndex];
-			video.play();
-		};
-		video.addEventListener("ended", handleEnded);
-		video.addEventListener("canplay", () => video.play());
+		const playVideo = () => video.play();
+		video.addEventListener("canplay", playVideo);
 		return () => {
-			video.removeEventListener("ended", handleEnded);
+			video.removeEventListener("canplay", playVideo);
 		};
-	}, [currentIndex, videos]);
+	}, []);
 
 	return (
 		<div className='home-container'>
 			<div className='video-container'>
 				<video
 					ref={videoRef}
-					src={videos[currentIndex]}
+					src={videoUrl}
 					autoPlay
+					loop
 					muted
 					playsInline
 					className='background-video'
